33c3fb90ccc8f905e20e205da87a7632359638f8
ELEONORA PROCOPIO s344405 <s344405@studenti.polito.it>
2025-10-12 18:54:25 +0200

    feat: added Routes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import TicketsPage from "./pages/TicketPage";
+import OfficerPage from "./pages/OfficerPage";
+import ManagerPage from "./pages/ManagerPage";
+import AdministratorPage from "./pages/AdministratorPage";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/tickets" element={<TicketsPage />} />
+        <Route path="/officer" element={<OfficerPage />} />
+        <Route path="/manager" element={<ManagerPage />} />
+        <Route path="/administrator" element={<AdministratorPage />} />
       </Routes>
     </Router>
   );
