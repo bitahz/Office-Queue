817730b727070253bc9e8a265b01893f5a716bae
Alessio Montel <montelalessio@gmail.com>
2025-10-13 14:36:46 +0200

    Add counters model

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ServicesModule } from './modules/services/services.module';
 import { TicketsModule } from './modules/tickets/tickets.module';
+import { CountersModule } from './modules/counters/counters.module';
 
 @Module({
-  imports: [ServicesModule, TicketsModule],
+  imports: [ServicesModule, TicketsModule, CountersModule],
 })
 export class AppModule {}
