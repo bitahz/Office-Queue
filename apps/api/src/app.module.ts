94b33970f474d10d30bcea68324a3b22d75c90ce
Alessio Montel <montelalessio@gmail.com>
2025-10-13 12:19:27 +0200

    Move ticket from module to modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServicesModule } from './modules/services/services.module';
-import { TicketModule } from './module/ticket/ticket.module';
+import { TicketModule } from './modules/ticket/ticket.module';
 
 @Module({
   imports: [ServicesModule, TicketModule],
