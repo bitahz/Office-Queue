031c01bd36023f1e0f5aa1f046ca0e44ddd5d083
Alessio Montel <montelalessio@gmail.com>
2025-10-13 12:38:23 +0200

    Add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,13 @@ async function bootstrap() {
   );
 
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Office Queue API')
+    .setVersion('0.0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.setGlobalPrefix('api');
 
