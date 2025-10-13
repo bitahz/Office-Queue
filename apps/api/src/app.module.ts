63598798cf8e309f7e96628240bd1fe5f190f5e7
Alessio Montel <montelalessio@gmail.com>
2025-10-13 12:20:00 +0200

    Remove boilerplate app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServicesModule } from './modules/services/services.module';
 import { TicketModule } from './modules/ticket/ticket.module';
 
 @Module({
   imports: [ServicesModule, TicketModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
